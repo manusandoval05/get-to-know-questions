c7aff72fafdb8c0ca7d6a376c56b60618333c721
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2022-10-09 14:13:26 -0500

    Added truth questions and backgrounds

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
-import { STARTERS, BASIC, FUN, EFFECTIVE, GOOD, INTERESTING, WORK, COUPLES, STUDENTS } from "./question_repository.js";
+import { STARTERS, BASIC, FUN, EFFECTIVE, GOOD, INTERESTING, WORK, COUPLES, STUDENTS, TRUTH } from "./question_repository.js";
+import { changeBackgroundColor } from "./background-color.js";
 
 const QUESTIONS = {
     starters: STARTERS, 
@@ -10,16 +11,19 @@ const QUESTIONS = {
     work: WORK, 
     couples: COUPLES, 
     students: STUDENTS, 
-    all: [].concat(STARTERS, BASIC, FUN, EFFECTIVE, GOOD, INTERESTING, WORK, COUPLES, STUDENTS),
+    truth: TRUTH,
+    all: [].concat(STARTERS, BASIC, FUN, EFFECTIVE, GOOD, INTERESTING, WORK, COUPLES, STUDENTS, TRUTH),
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const $buttonGenerator = document.getElementById("question-generator");
+    const $background = document.getElementById("background"); 
     const $dropdownFilter = document.getElementById("filter-dropdown");
     const $question = document.getElementById("current-question");
     $buttonGenerator.addEventListener("click", () => {
         const category = QUESTIONS[$dropdownFilter.value]; 
         const randomQuestion = category[Math.floor(Math.random() * category.length)];
         $question.innerText = randomQuestion; 
+        changeBackgroundColor($background);
     })
 })
