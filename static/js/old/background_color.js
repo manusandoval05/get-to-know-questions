8ee24887f3dc3de25297751c78d28773b26681bb
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2022-12-24 19:18:23 -0600

    Deleted previous code

--- static/js/old/background_color.js
+++
@@ -1,8 +0,0 @@
-const COLORS = ["is-primary", "is-link", "is-success", "is-warning", "is-danger"]
-let currentColor = "is-danger"; 
-
-export function changeBackgroundColor($background){
-    $background.classList.remove(currentColor);
-    currentColor = COLORS[Math.floor(Math.random() * COLORS.length)];
-    $background.classList.add(currentColor);
-}
--- static/js/old/index.js
+++
@@ -1,8 +0,0 @@
-import { initiateDeleteButton } from "./components/delete_button.js";
-import { initiateModals } from "./components/modal.js";
-import { initiateStartGameButton } from "./components/start_game_button.js";
-
-initiateDeleteButton(".field", ".delete-player");
-initiateModals(); 
-//Button that waits to the start game button to be pressed and then call the gameEventSystem() function.
-initiateStartGameButton(); 
